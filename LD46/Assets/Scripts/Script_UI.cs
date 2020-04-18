dab75abd30f4a6a45eee6a9d6e72f9911709fc89
MysteriouBaboon <MysteriousBaboon@hotmail.com>
2020-04-18 05:00:26 +0200

    TitleScreen starting the code

---
+++ LD46/Assets/Scripts/Script_UI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Script_UI : MonoBehaviour
+{
+    public string LevelName;
+
+    public void GotoScene()
+    {
+        SceneManager.LoadScene(LevelName);
+    }
+
+}
